5e188ca1ba403bd7015005996f4526dc60ae21ba
Willane Paiva <willanepaiva@hotmail.com>
2025-03-27 09:52:53 -0300

    add options screen

---
+++ app/src/main/java/br/edu/utfpr/comparacombustiveis/ListaCombustiveis.kt
@@ -0,0 +1,35 @@
+package br.edu.utfpr.comparacombustiveis
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.utfpr.comparacombustiveis.databinding.ActivityListaCombustiveisBinding
+
+class ListaCombustiveis : AppCompatActivity() {
+    private lateinit var binding: ActivityListaCombustiveisBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityListaCombustiveisBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.listaCombustiveis.setOnItemClickListener { _, _, position, _ ->
+            val consumo = when (position) {
+                0 -> 12.9
+                1 -> 9.1
+                else -> 0.0
+            }
+
+            val nome = when (position) {
+                0 -> "Gasolina"
+                else -> "Alcóol"
+            }
+            intent.putExtra("opcaoNome", nome)
+            intent.putExtra("opcaoConsumo", consumo)
+
+            setResult(RESULT_OK, intent)
+
+            finish()
+        }
+
+    }
+}
--- app/src/main/java/br/edu/utfpr/comparacombustiveis/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/comparacombustiveis/MainActivity.kt
@@ -1,11 +1,54 @@
 package br.edu.utfpr.comparacombustiveis
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.utfpr.comparacombustiveis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var opcaoAtual: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.opcao1Bt.setOnClickListener {
+            opcaoAtual = 1
+            onOpcaoClick()
+        }
+
+        binding.opcao2Bt.setOnClickListener {
+            opcaoAtual = 2
+            onOpcaoClick()
+        }
     }
+
+    private fun onOpcaoClick() {
+        val intent = Intent(this, ListaCombustiveis::class.java)
+
+        getOpcao.launch(intent)
+    }
+
+    private val getOpcao =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val opcao = it.data?.getStringExtra("opcaoNome")
+                val consumo = it.data?.getDoubleExtra("opcaoConsumo", 0.0)
+
+                if (opcaoAtual == 1) {
+                    binding.valueOpcao1.text = opcao
+                    binding.opcao1Consumo.text = getString(R.string.opcao_1_consumo, consumo.toString())
+                }
+                if (opcaoAtual == 2) {
+                    binding.valueOpcao2.text = opcao
+                    binding.opcao2Consumo.text = getString(R.string.opcao_2_consumo, consumo.toString())
+                }
+
+                opcaoAtual = 0
+            }
+        }
+
 }
